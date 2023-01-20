09fbd1f291fbba266e6736b8b2a2e8a35af0bd7b
E Luckie <eluckie.d@gmail.com>
2023-01-20 11:49:07 -0700

    refactor beepBoop() to take out unnecessary repetition

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@
 function beepBoop(n) {
   let numbers = [];
   for (let i = 0; i <= n; i++) {
-    if (i === 1 || i.toString().includes(1) && !i.toString().includes(2) && !i.toString().includes(3)) {
+    if (i.toString().includes(1) && !i.toString().includes(2) && !i.toString().includes(3)) {
       let i = "Beep!";
       numbers.push(i);
-    } else if (i === 2 || i.toString().includes(2) && !i.toString().includes(3)) {
+    } else if (i.toString().includes(2) && !i.toString().includes(3)) {
       let i = "Boop!";
       numbers.push(i);
-    } else if (i === 3 || i.toString().includes(3)) {
+    } else if (i.toString().includes(3)) {
       let i = "Won't you be my neighbor?";
       numbers.push(i);
     } else {
@@ -18,9 +18,7 @@ function beepBoop(n) {
   return numbers;
 }
 
-
 // ui logic
-
 window.onload = function() {
   document.querySelector("form#number-form").onsubmit = function(event) {
     event.preventDefault();
